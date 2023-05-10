3a812cce0ef6250d87e447b334c34e3dbb21af22
lojnys <lojnys@students.cs.ubc.ca>
2023-05-10 02:04:37 -0700

    "05/10/23 - initial commit"

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Work Hour Recorder
+
+## Description
+This application helps user keep track of the number of hours that they have worked.
+
+## User Story
+- As an user, I want to be able to number of hours worked, with corresponding hourly rate.
+- As an user, I want to be able to add up number of hours, calculated by bi-monthly. 
---
+++ model/WorkTime.py
@@ -0,0 +1,38 @@
+
+
+class WorkTime():
+    
+
+    # work time slot is created with given hours, rate, and any notes
+    def __init__(self, hours: float, rate: float, note: str, date) -> None:
+        self.hours = hours
+        self.rate = rate
+        self.note = note
+        self.date = date
+
+
+    # returns each hour, rate, and note
+    def getHours(self) -> float:
+        return self.hours
+    
+    def getRate(self) -> float:
+        return self.rate
+    
+    def getNote(self) -> str:
+        return self.note
+    
+    def getDate(self):
+        return self.date
+    
+    # sets each hours, rate, and note with given values
+    def setHours(self, hours: float) -> None:
+        self.hours = hours
+
+    def setRate(self, rate: float) -> None:
+        self.rate = rate
+
+    def setNote(self, note: str) -> None:
+        self.note = note
+
+    def setDate(self, date):
+        self.date = date
